df77ddae5e1a2902d9408d491007e7f70752f2a3
Andrei Vivar <drevivar2001@gmail.com>
2025-01-08 08:59:40 -0800

    deleted stock analysis folder

--- stockanalysis/src/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-SEC_API_API_KEY = os.getenv("SEC_API_API_KEY")
-SERPER_API_KEY = os.getenv("SERPER_API_KEY")
