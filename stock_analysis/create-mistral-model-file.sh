b7aab7af4552c0e6a8e46fb7b659fe92cce8080f
Andrei Vivar <drevivar2001@gmail.com>
2025-01-26 16:12:38 -0800

    change to Mistral

--- stock_analysis/create-mistral-model-file.sh
+++ stock_analysis/create-mistral-model-file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-model_name="mixtral"
-custom_model_name="crewai-mixtral"
+model_name="mistral"
+custom_model_name="crewai-mistral"
 
 ollama pull $model_name
 
-ollama create $custom_model_name -f ./MixtralModelFile
+ollama create $custom_model_name -f ./MistralModelFile
